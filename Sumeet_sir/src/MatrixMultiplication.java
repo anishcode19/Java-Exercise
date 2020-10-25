4bb6a236cd80d6e3e884449604f9f7f8e0f1f84a
4neesc0des <aneeskuar@gmail.com>
2020-10-25 23:50:49 +0530

    16th commit

--- Sumeet_sir/src/MatrixMultiplication.java
+++ Sumeet_sir/src/MatrixMultiplication.java
@@ -23,9 +23,22 @@ class MatrixMultiplication {
         }
         int[][] ans=new int[n1][m2];
         if(m1==n2){
-            
+            for (int i = 0; i < ans.length; i++) {
+                for (int j = 0; j < ans[0].length; j++) {
+                    for (int k = 0; k < m1; k++) {
+                        ans[i][j]+= arr1[i][k] * arr2[k][j];
+                    }
+                }
+            }
         }else{
             System.out.println("invalid input");
+        }
+        
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
         }
 }
 }
---
+++ Sumeet_sir/src/WaveTraversal.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+
+public class WaveTraversal {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int r =scn.nextInt();
+        int c =scn.nextInt();
+        
+        int arr[][] =new int[r][c];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        for (int j = 0; j < arr[0].length; j++) {
+            if(j%2==0){
+                for (int i = 0; i < arr.length; i++) {
+                    System.out.print(arr[i][j]+" ");
+                }
+            }else{
+                for (int i = arr.length-1; i> 0; i--) {
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
