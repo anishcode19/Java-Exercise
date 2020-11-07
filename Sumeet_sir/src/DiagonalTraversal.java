2f0ef82f1ed78eebaa66f9ccaf0db619cee7c071
4neesc0des <aneeskuar@gmail.com>
2020-11-07 18:53:27 +0530

    diagonal Traversal

---
+++ Sumeet_sir/src/DiagonalTraversal.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class DiagonalTraversal {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        for (int g = 0; g < arr.length; g++) {
+            for (int i=0,j = g; j < arr.length; i++, j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+        
+        
+    }
+}
